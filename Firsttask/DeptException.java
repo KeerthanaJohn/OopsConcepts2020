6db0f32e9ca40f93af83b83cee46bd8162142337
KeerthanaJohn <66940526+KeerthanaJohn@users.noreply.github.com>
2021-03-26 10:48:46 +0530

    Delete Firsttask directory

--- Firsttask/DeptException.java
+++
@@ -1,7 +0,0 @@
-package Firsttask;
-
-public class DeptException extends Exception{
-	public DeptException() {
-		System.out.println("Check your department");
-	}
-}
--- Firsttask/IdException.java
+++
@@ -1,9 +0,0 @@
-package Firsttask;
-
-public class IdException extends Exception{
-	
-	public IdException() {
-		System.out.println("Check staff id");
-	}
-
-}
